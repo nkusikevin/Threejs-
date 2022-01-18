b680b44f99acc2a3576069bc25858737d4b6a97e
nkusikevin <nkusikvnhart@gmail.com>
2022-01-18 20:03:59 +0200

    adding mouse animations

--- 07-cameras/src/script.js
+++ 07-cameras/src/script.js
@@ -1,6 +1,16 @@
 import "./style.css";
 import * as THREE from "three";
 
+//Cursor
+const cursor = {
+	x: 0,
+	y: 0,
+};
+window.addEventListener("mousemove", (e) => {
+	cursor.x = e.clientX / sizes.width - 0.5;
+	cursor.y = (e.clientY / sizes.height - 0.5) * -1
+});
+
 /**
  * Base
  */
@@ -19,29 +29,29 @@ const scene = new THREE.Scene();
 // Object
 const mesh = new THREE.Mesh(
 	new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-	new THREE.MeshBasicMaterial({ color: 0xff0000 })
+	new THREE.MeshBasicMaterial({ color: "green" })
 );
 scene.add(mesh);
 
 // Camera
-// const camera = new THREE.PerspectiveCamera(
-// 	75,
-// 	sizes.width / sizes.height,
-// 	0.01,
-// 	100
-// );
-const aspectRatio = sizes.width / sizes.height;
-const camera = new THREE.OrthographicCamera(
-	-1 * aspectRatio,
-	1 * aspectRatio,
-	1,
-	-1,
+const camera = new THREE.PerspectiveCamera(
+	75,
+	sizes.width / sizes.height,
 	0.01,
 	100
 );
-camera.position.x = 2;
-camera.position.y = 2;
-camera.position.z = 2;
+// const aspectRatio = sizes.width / sizes.height;
+// const camera = new THREE.OrthographicCamera(
+// 	-1 * aspectRatio,
+// 	1 * aspectRatio,
+// 	1,
+// 	-1,
+// 	0.01,
+// 	100
+// );
+// camera.position.x = 2;
+// camera.position.y = 2;
+camera.position.z = 3;
 camera.lookAt(mesh.position);
 scene.add(camera);
 
@@ -56,9 +66,13 @@ const clock = new THREE.Clock();
 
 const tick = () => {
 	const elapsedTime = clock.getElapsedTime();
-
+	//Update camera
+	camera.position.x = Math.sin(cursor.x * Math.PI) * 2;
+    camera.position.z = Math.cos(cursor.x * Math.PI) * 2;
+    camera.position.y = cursor.y * 3
+    camera.lookAt(mesh.position);
 	// Update objects
-	mesh.rotation.y = elapsedTime;
+	// mesh.rotation.y = elapsedTime;
 
 	// Render
 	renderer.render(scene, camera);

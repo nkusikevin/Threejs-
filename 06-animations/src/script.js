794f7e8663c998f1b7433954ead194aac38d4b0e
nkusikevin <nkusikvnhart@gmail.com>
2022-01-18 09:46:16 +0200

    adding animation with a boost frame rate per sec

--- 06-animations/src/script.js
+++ 06-animations/src/script.js
@@ -1,32 +1,52 @@
-import './style.css'
-import * as THREE from 'three'
+import "./style.css";
+import * as THREE from "three";
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "blue" });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
 // Sizes
 const sizes = {
-    width: 800,
-    height: 600
-}
+	width: 800,
+	height: 600,
+};
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+	canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+// renderer.render(scene, camera)
+
+//current Time
+let time = Date.now();
+
+//animations
+const tick = () => {
+
+	//Time
+	const currentTime = Date.now();
+	const deltaTime = currentTime - time;
+	time = currentTime;
+
+	
+	requestAnimationFrame(tick);
+	mesh.rotation.x += 0.001 * deltaTime;
+	mesh.rotation.y += 0.001 * deltaTime;
+	renderer.render(scene, camera);
+};
+
+tick();

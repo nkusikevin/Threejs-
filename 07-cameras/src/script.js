f62f52a8b8ea8abae23ebe72f4261f82f76b502c
nkusikevin <nkusikvnhart@gmail.com>
2022-01-18 11:41:39 +0200

    working with orthographicCamera

--- 07-cameras/src/script.js
+++ 07-cameras/src/script.js
@@ -24,9 +24,18 @@ const mesh = new THREE.Mesh(
 scene.add(mesh);
 
 // Camera
-const camera = new THREE.PerspectiveCamera(
-	75,
-	sizes.width / sizes.height,
+// const camera = new THREE.PerspectiveCamera(
+// 	75,
+// 	sizes.width / sizes.height,
+// 	0.01,
+// 	100
+// );
+const aspectRatio = sizes.width / sizes.height;
+const camera = new THREE.OrthographicCamera(
+	-1 * aspectRatio,
+	1 * aspectRatio,
+	1,
+	-1,
 	0.01,
 	100
 );

3cb7bdc963a25c09b30fa048204d9e85bef4d7af
nkusikevin <nkusikvnhart@gmail.com>
2022-01-24 10:06:18 +0200

    adding ambient and direaction lights[C

--- 15-lights/src/script.js
+++ 15-lights/src/script.js
@@ -18,14 +18,16 @@ const scene = new THREE.Scene()
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+const ambientLight = new THREE.AmbientLight()
+ambientLight.color =  new THREE.Color(0xffffff)
+ambientLight.intensity = 0.5
 scene.add(ambientLight)
 
-const pointLight = new THREE.PointLight(0xffffff, 0.5)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
+const directionalLight  = new THREE.DirectionalLight(0x00fffc,0.3)
+scene.add(directionalLight)
+
+
+gui.add(ambientLight,'intensity').min(0).max(1).step(0.01)
 
 /**
  * Objects

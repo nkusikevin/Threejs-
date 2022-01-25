8532700cde8287b032fb803be21b1688178bd8df
nkusikevin <nkusikvnhart@gmail.com>
2022-01-25 11:17:21 +0200

    adding the bushes

--- 17-haunted-house/src/script.js
+++ 17-haunted-house/src/script.js
@@ -50,6 +50,33 @@ new THREE.PlaneBufferGeometry(2,2),
 new THREE.MeshStandardMaterial({color:'#aa7b7b'})
 )
 
+//Bushes
+
+const bushGeometry = new THREE.SphereBufferGeometry(1,16,16)
+const bushMaterial = new THREE.MeshStandardMaterial({color:'#89c854'})
+
+
+const bush1 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush1.scale.set(0.5, 0.5, 0.5);
+bush1.position.set(0.8, 0.2, 2.2);
+
+
+const bush2 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush2.scale.set(0.25, 0.25, 0.25);
+bush2.position.set(1.4, 0.1, 2.1);
+
+const bush3 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush3.scale.set(0.4, 0.4, 0.4);
+bush3.position.set(-0.8, 0.1, 2.2);
+
+const bush4 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush4.scale.set(0.15, 0.15, 0.15);
+bush4.position.set(-1, 0.05, 2.6);
+
+house.add(bush1, bush2, bush3, bush4);
+
+
+
 door.position.y =1
 door.position.z = 2 +0.01
 house.add(door)

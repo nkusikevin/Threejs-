a3f2f8c8f6896c15c9db831b777157b21acb20f2
nkusikevin <nkusikvnhart@gmail.com>
2022-02-02 20:24:15 +0200

    final galaxy

--- 19-galaxy-generator/src/script.js
+++ 19-galaxy-generator/src/script.js
@@ -46,6 +46,10 @@ const generateGalaxy = () => {
 	const positions = new Float32Array(parameters.count * 3);
 	const colors = new Float32Array(parameters.count * 3);
 
+
+	const colorInside = new THREE.Color(parameters.insideColor);
+	const colorOutside = new THREE.Color(parameters.outsideColor);
+
 	for (let i = 0; i < parameters.count; i++) {
 		const i3 = i * 3;
 
@@ -76,20 +80,18 @@ const generateGalaxy = () => {
 		positions[i3 + 1] = randomY;
 		positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius + randomZ;
 
-
 		//colors
-
-		colors[i3] = 1;
-		colors[i3 + 1] = 0;
-		colors[i3 + 2] = 0;
+		const mixedColor = colorInside.clone();
+		mixedColor.lerp(colorOutside, radius / parameters.radius);
+		colors[i3] = mixedColor.r;
+		colors[i3 + 1] = mixedColor.g;
+		colors[i3 + 2] = mixedColor.b;
 	}
 
 	geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
 	geometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
 
 
-
-	
 	/**
 	 * Material
 	 */

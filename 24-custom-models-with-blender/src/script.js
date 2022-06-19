012eb29a1f3fe834d9fc4d35ee0f1b817834787c
nkusikevin <nkusikvnhart@gmail.com>
2022-06-19 22:14:17 +0200

    removing draco

--- 24-custom-models-with-blender/src/script.js
+++ 24-custom-models-with-blender/src/script.js
@@ -2,7 +2,7 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
+
 import * as dat from "lil-gui";
 
 /**
